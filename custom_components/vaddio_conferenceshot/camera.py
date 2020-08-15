7ae87e2d2f78f76ed5a67f96de483eb68b6bee73
Rohan Kapoor <rohan@rohankapoor.com>
2020-08-14 21:24:58 -0700

    Rewrite VaddioCamera entity to be based off Camera instead of GenericCamera

--- custom_components/vaddio_conferenceshot/camera.py
+++ custom_components/vaddio_conferenceshot/camera.py
@@ -1,8 +1,9 @@
-from homeassistant.components.generic.camera import (
+from homeassistant.components.camera import (
     CONF_STILL_IMAGE_URL,
     CONF_STREAM_SOURCE,
-    GenericCamera,
+    Camera,
     PLATFORM_SCHEMA,
+    SUPPORT_STREAM
 )
 from homeassistant.const import CONF_NAME, CONF_PLATFORM
 
@@ -19,21 +20,38 @@ async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     return True
 
 
-class VaddioCamera(GenericCamera):
+class VaddioCamera(Camera):
     """Representation of a Vaddio Camera."""
 
-    def __init__(self, hass, vaddio_device):
+    def __init__(self, vaddio_device):
         """Initialize as a subclass of GenericCamera."""
-        camera_url = "rtsp://{}:554{}".format(
+        super().__init__()
+        self._name = vaddio_device.name
+        self._stream_source = "rtsp://{}:554{}".format(
             vaddio_device._hostname, vaddio_device._path
         )
 
-        device_info = PLATFORM_SCHEMA(
-            {
-                CONF_PLATFORM: VADDIO_DOMAIN,
-                CONF_NAME: vaddio_device.name,
-                CONF_STILL_IMAGE_URL: camera_url,
-                CONF_STREAM_SOURCE: camera_url,
-            }
-        )
-        super().__init__(hass, device_info)
+    @property
+    def supported_features(self):
+        """Return supported features for this camera."""
+        return SUPPORT_STREAM
+
+    def camera_image(self):
+        """Return bytes of camera image."""
+        return None
+
+    @property
+    def name(self):
+        """Return the name of this device."""
+        return self._name
+
+    async def stream_source(self):
+        """Return the source of the stream."""
+        if self._stream_source is None:
+            return None
+
+        try:
+            return self._stream_source.async_render()
+        except TemplateError as err:
+            _LOGGER.error("Error parsing template %s: %s", self._stream_source, err)
+            return None
